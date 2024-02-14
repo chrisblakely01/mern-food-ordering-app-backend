3f8e93f2adafc2b20ae81efca7414ef23a0a2675
Chris Blakely <chrisblakely01@users.noreply.github.com>
2024-02-14 12:47:38 +0000

    adding example .env file

--- src/models/restaurant.ts
+++ src/models/restaurant.ts
@@ -1,7 +1,11 @@
 import mongoose, { InferSchemaType } from "mongoose";
 
 const menuItemSchema = new mongoose.Schema({
-  _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    default: () => new mongoose.Types.ObjectId(),
+  },
   name: { type: String, required: true },
   price: { type: Number, required: true },
 });
